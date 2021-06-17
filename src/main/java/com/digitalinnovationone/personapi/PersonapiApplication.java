d9b5379e30f8ae8c411c56f21160243b08169f9b
Rickson Pierre <83503463+ricksonpierre@users.noreply.github.com>
2021-06-17 18:44:53 -0300

    update in system.properties

--- src/main/java/com/digitalinnovationone/personapi/PersonapiApplication.java
+++ src/main/java/com/digitalinnovationone/personapi/PersonapiApplication.java
@@ -9,5 +9,4 @@ public class PersonapiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PersonapiApplication.class, args);
 	}
-
 }
--- src/main/java/com/digitalinnovationone/personapi/controller/PersonController.java
+++ src/main/java/com/digitalinnovationone/personapi/controller/PersonController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.digitalinnovationone.personapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +10,7 @@ public class PersonController {
 
     @GetMapping
     public String getBook(){
+
         return "API Test ativado!";
     }
 }
